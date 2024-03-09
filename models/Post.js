910d166ed9d08db2db438283cd0de4577eefe28b
Huseinhij <HuseinHijazi31@gmail.com>
2024-03-09 11:59:23 +0200

    fixed the posts

--- models/Post.js
+++ models/Post.js
@@ -1,11 +1,13 @@
+// models/Comment.js
 const mongoose = require('mongoose');
 
+// models/Post.js
 const postSchema = new mongoose.Schema({
-  content: String,
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
   createdAt: { type: Date, default: Date.now },
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 });
 
 const Post = mongoose.model('Post', postSchema);
-
 module.exports = Post;
